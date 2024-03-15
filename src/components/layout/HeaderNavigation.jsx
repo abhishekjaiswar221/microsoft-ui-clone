5812bdb83730f09da10dc40ffd922ac52ab8cb70
Abhishek D Jaiswar <devabhishekjaiswar@gmail.com>
2024-03-15 22:49:09 +0530

    Navbar for desktop screen is also complete

--- src/components/layout/HeaderNavigation.jsx
+++ src/components/layout/HeaderNavigation.jsx
@@ -12,26 +12,29 @@ import microsoftLogo from "../../assets/images/microsoft-logo.png";
 
 const HeaderNavigation = () => {
   const [menuBar, setMenuBar] = useState(false);
-  // console.log(menuBar);
+
   const toggleMenu = () => {
-    setMenuBar(!menuBar); //Toggle the value of menuBar variable-->If true then ~false or true if false  then ~true
-    // console.log(menuBar);
+    setMenuBar(!menuBar); // Toggle the value of menuBar[State]-->If true then ~true-->false if false  then ~false-->true
   };
+
   return (
     <>
       {/* Navigation for mobile */}
-      <div>
+      <div className="block full-laptop:hidden">
         <header>
-          <nav className="flex items-center border-b border-[#e6e6e6] justify-around w-full h-12 bg-white ">
-            <div onClick={toggleMenu}>
-              {menuBar ? ( //condition
-                <X size={20} strokeWidth={1.5} /> //true-->clicked
-              ) : (
-                <Menu size={20} strokeWidth={1.5} /> //false-->not clicked
-              )}
-            </div>
-            <div>
-              <Search size={20} strokeWidth={1.5} />
+          <nav className="flex items-center border-b border-[#e6e6e6] justify-around full-tablet:justify-between full-tablet:px-5 w-full h-12 bg-white ">
+            <div className="flex items-center justify-center gap-5">
+              <div onClick={toggleMenu}>
+                {/* Ternary Operator-->Condition ? True : False */}
+                {menuBar ? ( //condition
+                  <X size={20} strokeWidth={1.5} /> //true-->clicked
+                ) : (
+                  <Menu size={20} strokeWidth={1.5} /> //false-->not clicked
+                )}
+              </div>
+              <div>
+                <Search size={20} strokeWidth={1.5} />
+              </div>
             </div>
             <div>
               <img
@@ -40,14 +43,17 @@ const HeaderNavigation = () => {
                 alt="Microsoft Logo"
               />
             </div>
-            <div>
-              <ShoppingCart size={20} strokeWidth={1.5} />
-            </div>
-            <div>
-              <CircleUserRound size={30} strokeWidth={1} />
+            <div className="flex items-center justify-center gap-5">
+              <div>
+                <ShoppingCart size={20} strokeWidth={1.5} />
+              </div>
+              <div>
+                <CircleUserRound size={30} strokeWidth={1} />
+              </div>
             </div>
           </nav>
 
+          {/* Conditional Rendering-->If menBar[State] in true then render the below code and if it's false then don't render */}
           {menuBar && (
             <nav className="bg-[#f2f2f2] border-b border-black">
               <ul className="flex flex-col">
@@ -68,7 +74,10 @@ const HeaderNavigation = () => {
                   {/* <NavLink to={"/surface"}>Surface</NavLink> */}Surface
                 </li>
                 <li className="flex items-center justify-start w-full px-5 border-b border-[#e6e6e6] h-14">
-                  {/* <NavLink to={"/xbox"}>X Box</NavLink> */}X Box
+                  {/* <NavLink to={"/xbox"}>X Box</NavLink> */}Xbox
+                </li>
+                <li className="flex items-center justify-start w-full px-5 border-b border-[#e6e6e6] h-14">
+                  {/* <NavLink to={"/support"}>Support</NavLink> */}Support
                 </li>
                 <li className="flex items-center justify-between w-full px-5 border-b border-[#e6e6e6] h-14">
                   {/* <NavLink to={"/software"}>Software</NavLink> */}Software
@@ -76,7 +85,7 @@ const HeaderNavigation = () => {
                 </li>
                 <li className="flex items-center justify-between w-full px-5 border-b border-[#e6e6e6] h-14">
                   {/* <NavLink to={"/pcdevices"}>PCs and Devices</NavLink> */}
-                  PCs and Devices
+                  PCs & Devices
                   <ChevronDown size={20} strokeWidth={1.5} />
                 </li>
                 <li className="flex items-center justify-between w-full px-5 border-b border-[#e6e6e6] h-14">
@@ -109,6 +118,75 @@ const HeaderNavigation = () => {
       </div>
 
       {/* Navigation for desktop */}
+      <div className="hidden full-laptop:block">
+        <header>
+          <nav className="flex items-center justify-around h-14 border-b border-[#e6e6e6]">
+            <div>
+              <img
+                className="h-[23px]"
+                src={microsoftLogo}
+                alt="Microsoft Logo"
+              />
+            </div>
+            <div>
+              <ul className="flex items-center justify-around gap-5">
+                <li>
+                  {/* <NavLink to={"/microsoft"}>Microsoft 365</NavLink> */}
+                  Microsoft 365
+                </li>
+                <li>{/* <NavLink to={"/teams"}>Teams</NavLink> */}Teams</li>
+                <li>
+                  {/* <NavLink to={"/copilot"}>Copilot</NavLink> */}Copilot
+                </li>
+                <li>
+                  {/* <NavLink to={"/windows"}>Windows</NavLink> */}Windows
+                </li>
+                <li>
+                  {/* <NavLink to={"/surface"}>Surface</NavLink> */}Surface
+                </li>
+                <li>{/* <NavLink to={"/xbox"}>X Box</NavLink> */}Xbox</li>
+                <li>
+                  {/* <NavLink to={"/support"}>Support</NavLink> */}Support
+                </li>
+              </ul>
+            </div>
+            <div className="flex items-center justify-center gap-4">
+              <div className="flex items-center justify-center gap-1">
+                <div>
+                  <p>All Microsoft</p>
+                </div>
+                <div>
+                  <ChevronDown size={20} strokeWidth={1.5} />
+                </div>
+              </div>
+              <div className="flex items-center justify-center gap-1">
+                <div className="hidden lg-laptop:block">
+                  <p>Search</p>
+                </div>
+                <div>
+                  <Search size={20} strokeWidth={1.5} />
+                </div>
+              </div>
+              <div className="flex items-center justify-center gap-1">
+                <div className="hidden lg-laptop:block">
+                  <p>Cart</p>
+                </div>
+                <div>
+                  <ShoppingCart size={20} strokeWidth={1.5} />
+                </div>
+              </div>
+              <div className="flex items-center justify-center gap-1">
+                <div className="hidden lg-laptop:block">
+                  <p>Sign in</p>
+                </div>
+                <div>
+                  <CircleUserRound size={30} strokeWidth={1} />
+                </div>
+              </div>
+            </div>
+          </nav>
+        </header>
+      </div>
     </>
   );
 };
