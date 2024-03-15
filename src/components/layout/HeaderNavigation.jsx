67ac85acdba9fc1a261d299f41b6506514314c5f
Abhishek D Jaiswar <devabhishekjaiswar@gmail.com>
2024-03-15 19:09:09 +0530

    Navbar for mobile screen is complete

--- src/components/layout/HeaderNavigation.jsx
+++ src/components/layout/HeaderNavigation.jsx
@@ -1,71 +1,114 @@
-import { Search, Menu, ShoppingCart, CircleUserRound } from "lucide-react";
+import { useState } from "react";
+import {
+  Search,
+  Menu,
+  ShoppingCart,
+  CircleUserRound,
+  X,
+  ChevronDown,
+} from "lucide-react";
 // import { NavLink } from "react-router-dom";
-import Logo from "src/assets/images/microsoft-logo.png"; //Error
+import microsoftLogo from "../../assets/images/microsoft-logo.png";
 
 const HeaderNavigation = () => {
+  const [menuBar, setMenuBar] = useState(false);
+  // console.log(menuBar);
+  const toggleMenu = () => {
+    setMenuBar(!menuBar); //Toggle the value of menuBar variable-->If true then ~false or true if false  then ~true
+    // console.log(menuBar);
+  };
   return (
     <>
       {/* Navigation for mobile */}
-      <div className="">
+      <div>
         <header>
-          <nav className="flex items-center justify-between h-12 bg-white border-b border-black">
-            <div>
-              <Menu />
-            </div>
-            {/* <ul>
-              <li>
-                <NavLink to={"/microsoft"}>Microsoft 365</NavLink>
-              </li>
-              <li>
-                <NavLink to={"/teams"}>Teams</NavLink>
-              </li>
-              <li>
-                <NavLink to={"/copilot"}>Copilot</NavLink>
-              </li>
-              <li>
-                <NavLink to={"/windows"}>Windows</NavLink>
-              </li>
-              <li>
-                <NavLink to={"/surface"}>Surface</NavLink>
-              </li>
-              <li>
-                <NavLink to={"/xbox"}>X Box</NavLink>
-              </li>
-              <li>
-                <NavLink to={"/software"}>Software</NavLink>
-              </li>
-              <li>
-                <NavLink to={"/pcdevices"}>PCs and Devices</NavLink>
-              </li>
-              <li>
-                <NavLink to={"/entertainment"}>Entertainment</NavLink>
-              </li>
-              <li>
-                <NavLink to={"/business"}>Business</NavLink>
-              </li>
-              <li>
-                <NavLink to={"/developer"}>Developer & IT</NavLink>
-              </li>
-              <li>
-                <NavLink to={"/other"}>Other</NavLink>
-              </li>
-              <li>
-                <NavLink to={"/sitemap"}>View Sitemap</NavLink>
-              </li>
-            </ul> */}
-            <div>
-              <Search />
-            </div>
-            <div>{Logo}</div>
-            <div>
-              <ShoppingCart />
+          <nav className="flex items-center border-b border-[#e6e6e6] justify-around w-full h-12 bg-white ">
+            <div onClick={toggleMenu}>
+              {menuBar ? ( //condition
+                <X size={20} strokeWidth={1.5} /> //true-->clicked
+              ) : (
+                <Menu size={20} strokeWidth={1.5} /> //false-->not clicked
+              )}
             </div>
             <div>
-              <CircleUserRound />
+              <Search size={20} strokeWidth={1.5} />
+            </div>
+            <div>
+              <img
+                className="h-[23px]"
+                src={microsoftLogo}
+                alt="Microsoft Logo"
+              />
+            </div>
+            <div>
+              <ShoppingCart size={20} strokeWidth={1.5} />
+            </div>
+            <div>
+              <CircleUserRound size={30} strokeWidth={1} />
             </div>
           </nav>
+
+          {menuBar && (
+            <nav className="bg-[#f2f2f2] border-b border-black">
+              <ul className="flex flex-col">
+                <li className="flex items-center justify-start w-full px-5 border-b border-[#e6e6e6] h-14">
+                  {/* <NavLink to={"/microsoft"}>Microsoft 365</NavLink> */}
+                  Microsoft 365
+                </li>
+                <li className="flex items-center justify-start w-full px-5 border-b border-[#e6e6e6] h-14">
+                  {/* <NavLink to={"/teams"}>Teams</NavLink> */}Teams
+                </li>
+                <li className="flex items-center justify-start w-full px-5 border-b border-[#e6e6e6] h-14">
+                  {/* <NavLink to={"/copilot"}>Copilot</NavLink> */}Copilot
+                </li>
+                <li className="flex items-center justify-start w-full px-5 border-b border-[#e6e6e6] h-14">
+                  {/* <NavLink to={"/windows"}>Windows</NavLink> */}Windows
+                </li>
+                <li className="flex items-center justify-start w-full px-5 border-b border-[#e6e6e6] h-14">
+                  {/* <NavLink to={"/surface"}>Surface</NavLink> */}Surface
+                </li>
+                <li className="flex items-center justify-start w-full px-5 border-b border-[#e6e6e6] h-14">
+                  {/* <NavLink to={"/xbox"}>X Box</NavLink> */}X Box
+                </li>
+                <li className="flex items-center justify-between w-full px-5 border-b border-[#e6e6e6] h-14">
+                  {/* <NavLink to={"/software"}>Software</NavLink> */}Software
+                  <ChevronDown size={20} strokeWidth={1.5} />
+                </li>
+                <li className="flex items-center justify-between w-full px-5 border-b border-[#e6e6e6] h-14">
+                  {/* <NavLink to={"/pcdevices"}>PCs and Devices</NavLink> */}
+                  PCs and Devices
+                  <ChevronDown size={20} strokeWidth={1.5} />
+                </li>
+                <li className="flex items-center justify-between w-full px-5 border-b border-[#e6e6e6] h-14">
+                  {/* <NavLink to={"/entertainment"}>Entertainment</NavLink> */}
+                  Entertainment
+                  <ChevronDown size={20} strokeWidth={1.5} />
+                </li>
+                <li className="flex items-center justify-between w-full px-5 border-b border-[#e6e6e6] h-14">
+                  {/* <NavLink to={"/business"}>Business</NavLink> */}
+                  Business
+                  <ChevronDown size={20} strokeWidth={1.5} />
+                </li>
+                <li className="flex items-center justify-between w-full px-5 border-b border-[#e6e6e6] h-14">
+                  {/* <NavLink to={"/developer"}>Developer & IT</NavLink> */}
+                  Developer & IT
+                  <ChevronDown size={20} strokeWidth={1.5} />
+                </li>
+                <li className="flex items-center justify-between w-full px-5 border-b border-[#e6e6e6] h-14">
+                  {/* <NavLink to={"/other"}>Other</NavLink> */}Other
+                  <ChevronDown size={20} strokeWidth={1.5} />
+                </li>
+                <li className="flex items-center justify-start w-full px-5 border-b border-[#e6e6e6] h-14">
+                  {/* <NavLink to={"/sitemap"}>View Sitemap</NavLink> */}View
+                  Sitemap
+                </li>
+              </ul>
+            </nav>
+          )}
         </header>
       </div>
+
+      {/* Navigation for desktop */}
     </>
   );
 };
